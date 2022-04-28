b800be9fa80b1a405112c5b3d396aec148947af4
LEESOOMIN94 <dltnals4128@naver.com>
2022-04-28 20:15:42 +0900

    2. ui 구성 : 화면 이동 설계

--- app/src/main/java/android/shopping/app/SplashActivity.kt
+++ app/src/main/java/android/shopping/app/SplashActivity.kt
@@ -1,12 +1,16 @@
 package android.shopping.app
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity: AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        startActivity(Intent(this,MainActivity::class.java))
+        finish()
+
     }
 }
